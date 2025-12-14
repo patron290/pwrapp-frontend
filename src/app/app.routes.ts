8e82e249e72358591034305ec1ba6d3df7def7bc
patron290 <balikapatrik@gmail.com>
2025-12-14 14:39:48 +0100

    feat: implement header, footer, and routing structure for the application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { CompetitorList } from './competitor-list/competitor-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'competitors',
+    component: CompetitorList,
+  },
+];
