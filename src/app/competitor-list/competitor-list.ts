b7be530594a1e2959a95d64942fca98f8feafa31
patron290 <balikapatrik@gmail.com>
2025-12-22 12:49:20 +0100

    feat: enhance competitor list component with data display and management functionality

--- src/app/competitor-list/competitor-list.ts
+++ src/app/competitor-list/competitor-list.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
-import { MatSlideToggle } from '@angular/material/slide-toggle';
+import { MatTableModule } from '@angular/material/table';
+import { MatButton } from '@angular/material/button';
+
+export interface Competitor {
+  name: string;
+  bodyWeight: number;
+  gender: 'male' | 'female';
+  age: number;
+}
+
+const competitors: Competitor[] = [
+  { name: 'Alice', bodyWeight: 65, gender: 'female', age: 28 },
+  { name: 'Bob', bodyWeight: 85, gender: 'male', age: 32 },
+  { name: 'Charlie', bodyWeight: 75, gender: 'male', age: 24 },
+];
 
 @Component({
   selector: 'app-competitor-list',
-  imports: [MatSlideToggle],
+  imports: [MatTableModule, MatButton],
   templateUrl: './competitor-list.html',
   styleUrl: './competitor-list.css',
 })
 export class CompetitorList {
+  displayedColumns: string[] = ['name', 'gender', 'age'];
+  dataSource = competitors;
 
+  addData() {}
+
+  removeData() {}
 }
